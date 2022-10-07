88efa27e94fd123d7413f70f62b1b23226c5a16d
SayHelloToKam <sayhello@kamilwatson.com>
2022-10-06 21:50:32 -0400

    finished functionality

--- src/App.js
+++ src/App.js
@@ -28,13 +28,32 @@ const getAllNames=()=>{
   setAllNames(data)
 }
 
+const addToTeam=(dev)=>{
+  let found = myTeam.some(item=>item.id === dev.id)
+
+  if(myTeam.length===6){
+    alert('You have reached the max number of devs allowed.')
+  } else if (!found) {
+    setMyTeam([...myTeam,dev])
+    let newList = allNames.filter(item=>item.id !== dev.id)
+    setAllNames(newList)
+  }
+  
+}
+
+const removeFromTeam=(dev)=>{
+  let updatedTeam = myTeam.filter(item=>item.id !== dev.id)
+  setMyTeam(updatedTeam)
+  setAllNames([dev,...allNames])
+}
+
   return (
     <div>
       <Input handleFilter={handleFilter} getAllNames={getAllNames} getOnlyFrontend={getOnlyFrontend} getOnlyBackend={getOnlyBackend}/>
-      <MyTeam myTeam={myTeam} />
+      <MyTeam myTeam={myTeam} removeFromTeam={removeFromTeam}/>
       <div>
       {allNames.map(item=> {
-        return <button className={item.speciality==="frontend" ? "frontend-dev" : "backend-dev"}>{item.name}</button>
+        return <button onClick={()=>addToTeam(item)} className={item.speciality==="frontend" ? "frontend-dev" : "backend-dev"}>{item.name}</button>
         })
       }
       </div>
--- src/MyTeam.js
+++ src/MyTeam.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function MyTeam({myTeam}) {
+export default function MyTeam({myTeam, removeFromTeam}) {
   return (
     <div className='myteam-container'>
        {
          myTeam.map(item=>{
-            return <button className={item.speciality==="frontend" ? "frontend-dev" : "backend-dev"}>{item.name}</button>
+            return <button onClick={()=>removeFromTeam(item)} className={item.speciality==="frontend" ? "frontend-dev" : "backend-dev"}>{item.name}</button>
         })
        }
     </div>
